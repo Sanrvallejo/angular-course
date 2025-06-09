7e8c38da253f6805121bf986438258c573b5fcc9
Sanrvallejo <zanti-23@hotmail.com>
2025-06-09 15:55:58 -0500

    @for directive and two-way binding

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -7,14 +7,41 @@ interface Character {
 }
 
 @Component({
-  imports: [],
-  templateUrl: './dragonball-page.component.html',
-  styleUrl: './dragonball-page.component.css',
+  templateUrl: './dragonball-page.component.html'
 })
 export class DragonballPageComponent {
+
+  name = signal('');
+  power = signal(0);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 3, name: 'Piccolo', power: 3000 },
   ]);
+
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+
+    //forma incorrecta de actualizar un señal de arreglo
+    // this.characters().push(newCharacter);
+
+    //forma correcta de actualizar una señal
+    //usar método update()
+    //usar operador rest para obtener lo que tiene el arreglo ...arreglo
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.name.set('');
+    this.power.set(0);
+  }
+
 }
